ba9a23d4ff63c4f552c7d2b5515f5aeacea4cb40
Alexandre Tucunduva <motive.mobi@gmail.com>
2020-05-09 08:29:29 -0300

    adicionado paginação nos forms

--- public/js/functions.js
+++ public/js/functions.js
@@ -64,6 +64,53 @@ $(document).ready(function() {
                   }
               });
 
+  //DATATABLES
+  const langOptions = {
+    "sEmptyTable": "Nenhum registro encontrado",
+    "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+    "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+    "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+    "sInfoPostFix": "",
+    "sInfoThousands": ".",
+    "sLengthMenu": "_MENU_ resultados por página",
+    "sLoadingRecords": "Carregando...",
+    "sProcessing": "Processando...",
+    "sZeroRecords": "Nenhum registro encontrado",
+    "sSearch": "Pesquisar",
+    "oPaginate": {
+        "sNext": "Próximo",
+        "sPrevious": "Anterior",
+        "sFirst": "Primeiro",
+        "sLast": "Último"
+    },
+    "oAria": {
+        "sSortAscending": ": Ordenar colunas de forma ascendente",
+        "sSortDescending": ": Ordenar colunas de forma descendente"
+    },
+    "select": {
+        "rows": {
+            "_": "Selecionado %d linhas",
+            "0": "Nenhuma linha selecionada",
+            "1": "Selecionado 1 linha"
+        }
+    }
+  };
+  
+  $('#pacientes').DataTable({
+    //"pageLength": 25,
+    "language": langOptions
+  });
+
+  $('#medicos').DataTable({
+    //"pageLength": 25,
+    "language": langOptions
+  });
+
+  $('#agentes').DataTable({
+    //"pageLength": 25,
+    "language": langOptions
+  });
+
   //JQUERY MASKS
   $('.date').mask('00/00/0000');
   //$('.time').mask('00:00:00');

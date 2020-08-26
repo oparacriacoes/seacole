d7eeb6acc6a0c54340979729eac4e9be6a47c296
Fristtram Helder Fernandes <fristtram@gmail.com>
2020-08-26 10:25:28 -0300

    Atualização dos names e IDs do form

--- app/Http/Controllers/API/PacienteController.php
+++ app/Http/Controllers/API/PacienteController.php
@@ -36,7 +36,7 @@ class PacienteController extends Controller
      */
     public function store(Request $request)
     {
-      dd($request);
+      dd($request->all());
       DB::beginTransaction();
       $user = new User;
       $user->name = $request->input('data')['name'];

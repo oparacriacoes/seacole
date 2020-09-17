a7d9604c27fb5ded77da4f780cd1e851cf05fa07
Alexandre Tucunduva <motive.mobi@gmail.com>
2020-09-17 10:18:16 -0300

    adicionado campo idade na exportação

--- app/Exports/PacientesExport.php
+++ app/Exports/PacientesExport.php
@@ -4,88 +4,106 @@ namespace App\Exports;
 
 use App\Paciente;
 use App\Articuladora;
+use Carbon\Carbon;
 //use Illuminate\Support\Facades\DB;
 //use Maatwebsite\Excel\Concerns\FromCollection;
-use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromView;
+//use Illuminate\Contracts\View\View;
+//use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use Maatwebsite\Excel\Concerns\WithTitle;
 
 //class PacientesExport implements FromCollection
 //class PacientesExport implements FromView
-class PacientesExport implements FromArray, WithHeadings
+class PacientesExport implements FromArray, WithHeadings, WithMultipleSheets, WithTitle
 {
     /**
     * @return \Illuminate\Support\Collection
     */
 
+    public function sheets(): array
+    {
+        $sheets = [
+          new PacientesExport(),
+        ];
+
+        return $sheets;
+    }
+
+    public function title(): string
+    {
+      return 'Pacientes';
+    }
+
     public function headings(): array
     {
         return [
-          'nome',
-          'name_social',
-          'identidade_genero',
-          'orientacao_sexual',
-          'agente',
-          'medico',
-          'articuladora',
-          'atendimento_semanal_psicologia',
-          'acompanhamento_psicologico',
-          'horario_at_psicologia',
-          'como_chegou_ao_projeto',
-          'nucleo_uneafro_qual',
-          'como_chegou_ao_projeto_outro',
-          'psicologo',
-          'situacao',
-          'data_nascimento',
-          'cor_raca',
-          'endereco_cep',
-          'endereco_rua',
-          'endereco_numero',
-          'endereco_bairro',
-          'endereco_cidade',
-          'endereco_uf',
-          'ponto_referencia',
-          'endereco_complemento',
-          'fone_fixo',
-          'fone_celular',
-          'numero_pessoas_residencia',
-          'responsavel_residencia',
-          'renda_residencia',
-          'doenca_cronica',
-          'descreve_doencas',
-          'remedios_consumidos',
-          'acompanhamento_medico',
-          'isolamento_residencial',
-          'alimentacao_disponivel',
-          'auxilio_terceiros',
-          'tarefas_autocuidado',
-          'teste_utilizado',
-          'resultado_teste ',
-          'outras_informacao',
-          'data_teste_confirmatorio ',
-          'data_inicio_sintoma',
-          'data_inicio_monitoramento',
-          'data_inicio_ac_psicologico',
-          'data_encerramento_ac_psicologico',
-          'data_finalizacao_caso',
-          'auxilio_emergencial',
-          'descreve_doencas',
-          'tuberculose',
-          'tabagista',
-          'cronico_alcool',
-          'outras_drogas',
-          'gestante',
-          'amamenta',
-          'gestacao_alto_risco',
-          'pos_parto',
-          'data_parto',
-          'data_ultima_mestrucao',
-          'trimestre_gestacao',
-          'motivo_risco_gravidez',
-          'data_ultima_consulta',
-          'sistema_saude',
-          'acompanhamento_ubs',
+          'Nome',
+          'Nome social',
+          'Identidade gênero',
+          'Orientação sexual',
+          'Agente',
+          'Médico',
+          'Articuladora',
+          'At. semanal psicol.',
+          'Acompanhamento psicol.',
+          'Hor. at. psicol.',
+          'Como chegou ao projeto',
+          'Núcleo UNEAFRO qual?',
+          'Como chegou ao projeto outro',
+          'Psicólogo',
+          'Situação',
+          'Data nascimento',
+          'Idade',
+          'Cor/Raça',
+          'CEP',
+          'Rua',
+          'Número',
+          'Bairro',
+          'Cidade',
+          'UF',
+          'Ponto referência',
+          'Complemento',
+          'Tel. fixo',
+          'Tel. celular',
+          'Nº pessoas residência',
+          'Responsável residência',
+          'Renda residência',
+          'Doença crônica',
+          'Descrição doenças crônicas',
+          'Remédios consumidos',
+          'Acompanhamento médico',
+          'Isolamento residencial',
+          'Alimentacao disponível',
+          'Auxílio terceiros',
+          'Tarefas autocuidado',
+          'Teste utilizado',
+          'Resultado teste',
+          'Outras inf. sobre o teste',
+          'Data teste confirmatório',
+          'Data início sintoma',
+          'Data início monitoramento',
+          'Data início ac. psicológico',
+          'Data encerramento ac. psicológico',
+          'Data finalização caso',
+          'Auxílio emergencial',
+          'Descrição doenças',
+          'Tuberculose',
+          'Tabagista',
+          'Alcool crônico',
+          'Outras drogas',
+          'Gestante',
+          'Amamenta',
+          'Gestação alto risco',
+          'Pós parto',
+          'Data parto',
+          'Data última mestruação',
+          'Trimestre gestacao',
+          'Motivo risco gravidez',
+          'Data última consulta',
+          'Sistema saúde',
+          'Acompanhamento UBS',
         ];
     }
 
@@ -121,6 +139,15 @@ class PacientesExport implements FromArray, WithHeadings
       ]);
     }
 
+    public function ageCalc($date)
+    {
+      $date_replace = str_replace('/', '-', $date);
+      $date_time = strtotime($date_replace);
+      $to_date = date('Y-m-d', $date_time);
+      $date_parse = Carbon::parse($to_date);
+      return $date_parse->diffInYears();
+    }
+
     public function array(): array
     {
       $pacientes = Paciente::get();
@@ -140,7 +167,6 @@ class PacientesExport implements FromArray, WithHeadings
 
         $doenca_cronica = [];
         $doenca = $paciente->doenca_cronica ? unserialize($paciente->doenca_cronica) : [];
-
         if(in_array('1', $doenca)){
           array_push($doenca_cronica, 'Hipertensão arterial sistêmica (HAS)');
         }
@@ -191,7 +217,6 @@ class PacientesExport implements FromArray, WithHeadings
         }
 
         $situacao_array = [];
-        //$situacao = $paciente->situacao ? unserialize($paciente->situacao) : [];
         if($paciente->situacao === '1'){
           array_push($situacao_array, 'Caso ativo GRAVE');
         }
@@ -235,74 +260,76 @@ class PacientesExport implements FromArray, WithHeadings
           array_push($situacao_array, 'Exclusivo psicologia - finalizado');
         }
 
+        if( $paciente->data_nascimento ){
+          $age = $this->ageCalc($paciente->data_nascimento);
+        }
+
         array_push($pacientes_array, [
-          'nome' => $paciente->user->name,
-          'name_social' => $paciente->name_social ? $paciente->name_social : '',
-          'identidade_genero' => $paciente->identidade_genero ? $paciente->identidade_genero : '',
-          'orientacao_sexual' => $paciente->orientacao_sexual ? $paciente->orientacao_sexual : '',
-          'agente' => $paciente->agente ? $paciente->agente->user->name : '',
-          'medico' => $paciente->medico ? $paciente->medico->user->name : '',
-          'articuladora' => $paciente->articuladora_responsavel ? Articuladora::where('id', $paciente->articuladora_responsavel)->first()->name : '',
-          'atendimento_semanal_psicologia' => $paciente->atendimento_semanal_psicologia ? $paciente->atendimento_semanal_psicologia : '',
-          'acompanhamento_psicologico' => $paciente->acompanhamento_psicologico ? implode(', ', unserialize($paciente->acompanhamento_psicologico)) : '',
-          'horario_at_psicologia' => $paciente->horario_at_psicologia ? $paciente->horario_at_psicologia : '',
-          'como_chegou_ao_projeto' => $paciente->como_chegou_ao_projeto ? $paciente->como_chegou_ao_projeto : '',
-          'nucleo_uneafro_qual' => $paciente->nucleo_uneafro_qual ? $paciente->nucleo_uneafro_qual : '',
-          'como_chegou_ao_projeto_outro' => $paciente->como_chegou_ao_projeto_outro ? $paciente->como_chegou_ao_projeto_outro : '',
-          'psicologo' => $paciente->psicologo ? $paciente->psicologo->user->name : '',
-          //'situacao' => $paciente->situacao ? $paciente->situacao : '',
-          'situacao' => implode(', ', $situacao_array),
-          'data_nascimento' => $paciente->data_nascimento ? $paciente->data_nascimento : '',
-          'cor_raca' => $paciente->cor_raca ? $paciente->cor_raca : '',
-          'endereco_cep' => $paciente->endereco_cep ? $paciente->endereco_cep : '',
-          'endereco_rua' => $paciente->endereco_rua ? $paciente->endereco_rua : '',
-          'endereco_numero' => $paciente->endereco_numero ? $paciente->endereco_numero : '',
-          'endereco_bairro' => $paciente->endereco_bairro ? $paciente->endereco_bairro : '',
-          'endereco_cidade' => $paciente->endereco_cidade ? $paciente->endereco_cidade : '',
-          'endereco_uf' => $paciente->endereco_uf ? $paciente->endereco_uf : '',
-          'ponto_referencia' => $paciente->ponto_referencia ? $paciente->ponto_referencia : '',
-          'endereco_complemento' => $paciente->endereco_complemento ? $paciente->endereco_complemento : '',
-          'fone_fixo' => $paciente->fone_fixo ? $paciente->fone_fixo : '',
-          'fone_celular' => $paciente->fone_celular ? $paciente->fone_celular : '',
-          'numero_pessoas_residencia' => $paciente->numero_pessoas_residencia ? $paciente->numero_pessoas_residencia : '',
-          'responsavel_residencia' => $paciente->responsavel_residencia ? $paciente->responsavel_residencia : '',
-          'renda_residencia' => $paciente->renda_residencia ? $paciente->renda_residencia : '',
-          //'doenca_cronica' => $paciente->doenca_cronica ? unserialize($paciente->doenca_cronica) : '',
-          'doenca_cronica' => implode(', ', $doenca_cronica),
-          'descreve_doencas' => $paciente->descreve_doencas ? $paciente->descreve_doencas : '',
-          'remedios_consumidos' => $paciente->remedios_consumidos ? $paciente->remedios_consumidos : '',
-          'acompanhamento_medico' => $paciente->acompanhamento_medico ? $paciente->acompanhamento_medico : '',
-          'isolamento_residencial' => $paciente->isolamento_residencial ? $paciente->isolamento_residencial : '',
-          'alimentacao_disponivel' => $paciente->alimentacao_disponivel ? $paciente->alimentacao_disponivel : '',
-          'auxilio_terceiros' => $paciente->auxilio_terceiros ? $paciente->auxilio_terceiros : '',
-          'tarefas_autocuidado' => $paciente->tarefas_autocuidado ? $paciente->tarefas_autocuidado : '',
-          //'teste_utilizado' => $paciente->teste_utilizado ? unserialize($paciente->teste_utilizado) : '',
-          'teste_utilizado' => $teste_utilizado,
-          'resultado_teste' => $resultado_teste ? $resultado_teste : '',
-          'outras_informacao' => $paciente->outras_informacao ? $paciente->outras_informacao : '',
-          'data_teste_confirmatorio' => $paciente->data_teste_confirmatorio ? $paciente->data_teste_confirmatorio : '',
-          'data_inicio_sintoma' => $paciente->data_inicio_sintoma ? $paciente->data_inicio_sintoma : '',
-          'data_inicio_monitoramento' => $paciente->data_inicio_monitoramento ? $paciente->data_inicio_monitoramento : '',
-          'data_inicio_ac_psicologico' => $paciente->data_inicio_ac_psicologico ? $paciente->data_inicio_ac_psicologico : '',
-          'data_encerramento_ac_psicologico' => $paciente->data_encerramento_ac_psicologico ? $paciente->data_encerramento_ac_psicologico : '',
-          'data_finalizacao_caso' => $paciente->data_finalizacao_caso ? $paciente->data_finalizacao_caso : '',
-          'auxilio_emergencial' => $paciente->auxilio_emergencial ? $paciente->auxilio_emergencial : '',
-          'descreve_doencas' => $paciente->descreve_doencas ? $paciente->descreve_doencas : '',
-          'tuberculose' => $paciente->tuberculose ? $paciente->tuberculose : '',
-          'tabagista' => $paciente->tabagista ? $paciente->tabagista : '',
-          'cronico_alcool' => $paciente->cronico_alcool ? $paciente->cronico_alcool : '',
-          'outras_drogas' => $paciente->outras_drogas ? $paciente->outras_drogas : '',
-          'gestante' => $paciente->gestante ? $paciente->gestante : '',
-          'amamenta' => $paciente->amamenta ? $paciente->amamenta : '',
-          'gestacao_alto_risco' => $paciente->gestacao_alto_risco ? $paciente->gestacao_alto_risco : '',
-          'pos_parto' => $paciente->pos_parto ? $paciente->pos_parto : '',
-          'data_parto' => $paciente->data_parto ? $paciente->data_parto : '',
-          'data_ultima_mestrucao' => $paciente->data_ultima_mestrucao ? $paciente->data_ultima_mestrucao : '',
-          'trimestre_gestacao' => $paciente->trimestre_gestacao ? $paciente->trimestre_gestacao : '',
-          'motivo_risco_gravidez' => $paciente->motivo_risco_gravidez ? $paciente->motivo_risco_gravidez : '',
-          'data_ultima_consulta' => $paciente->data_ultima_consulta ? $paciente->data_ultima_consulta : '',
-          'sistema_saude'  => $paciente->sistema_saude ? implode(', ', unserialize($paciente->sistema_saude)) : '',
-          'acompanhamento_ubs' => $paciente->acompanhamento_ubs ? $paciente->acompanhamento_ubs : '',
+          'Nome' => $paciente->user->name,
+          'Nome social' => $paciente->name_social ? $paciente->name_social : '',
+          'Identidade gênero' => $paciente->identidade_genero ? $paciente->identidade_genero : '',
+          'Orientação sexual' => $paciente->orientacao_sexual ? $paciente->orientacao_sexual : '',
+          'Agente' => $paciente->agente ? $paciente->agente->user->name : '',
+          'Médico' => $paciente->medico ? $paciente->medico->user->name : '',
+          'Articuladora' => $paciente->articuladora_responsavel ? Articuladora::where('id', $paciente->articuladora_responsavel)->first()->name : '',
+          'At. semanal psicol.' => $paciente->atendimento_semanal_psicologia ? $paciente->atendimento_semanal_psicologia : '',
+          'Acompanhamento psicol.' => $paciente->acompanhamento_psicologico ? implode(', ', unserialize($paciente->acompanhamento_psicologico)) : '',
+          'Hor. at. psicol.' => $paciente->horario_at_psicologia ? $paciente->horario_at_psicologia : '',
+          'Como chegou ao projeto' => $paciente->como_chegou_ao_projeto ? $paciente->como_chegou_ao_projeto : '',
+          'Núcleo UNEAFRO qual?' => $paciente->nucleo_uneafro_qual ? $paciente->nucleo_uneafro_qual : '',
+          'Como chegou ao projeto outro' => $paciente->como_chegou_ao_projeto_outro ? $paciente->como_chegou_ao_projeto_outro : '',
+          'Psicólogo' => $paciente->psicologo ? $paciente->psicologo->user->name : '',
+          'Situação' => implode(', ', $situacao_array),
+          'Data nascimento' => $paciente->data_nascimento ? $paciente->data_nascimento : '',
+          'Idade' => $age,
+          'Cor/Raça' => $paciente->cor_raca ? $paciente->cor_raca : '',
+          'CEP' => $paciente->endereco_cep ? $paciente->endereco_cep : '',
+          'Rua' => $paciente->endereco_rua ? $paciente->endereco_rua : '',
+          'Número' => $paciente->endereco_numero ? $paciente->endereco_numero : '',
+          'Bairro' => $paciente->endereco_bairro ? $paciente->endereco_bairro : '',
+          'Cidade' => $paciente->endereco_cidade ? $paciente->endereco_cidade : '',
+          'UF' => $paciente->endereco_uf ? $paciente->endereco_uf : '',
+          'Ponto referência' => $paciente->ponto_referencia ? $paciente->ponto_referencia : '',
+          'Complemento' => $paciente->endereco_complemento ? $paciente->endereco_complemento : '',
+          'Tel. fixo' => $paciente->fone_fixo ? $paciente->fone_fixo : '',
+          'Tel. celular' => $paciente->fone_celular ? $paciente->fone_celular : '',
+          'Nº pessoas residência' => $paciente->numero_pessoas_residencia ? $paciente->numero_pessoas_residencia : '',
+          'Responsável residência' => $paciente->responsavel_residencia ? $paciente->responsavel_residencia : '',
+          'Renda residência' => $paciente->renda_residencia ? $paciente->renda_residencia : '',
+          'Doença crônica' => implode(', ', $doenca_cronica),
+          'Descrição doenças crônicas' => $paciente->descreve_doencas ? $paciente->descreve_doencas : '',
+          'Remédios consumidos' => $paciente->remedios_consumidos ? $paciente->remedios_consumidos : '',
+          'Acompanhamento médico' => $paciente->acompanhamento_medico ? $paciente->acompanhamento_medico : '',
+          'Isolamento residencial' => $paciente->isolamento_residencial ? $paciente->isolamento_residencial : '',
+          'Alimentacao disponível' => $paciente->alimentacao_disponivel ? $paciente->alimentacao_disponivel : '',
+          'Auxílio terceiros' => $paciente->auxilio_terceiros ? $paciente->auxilio_terceiros : '',
+          'Tarefas autocuidado' => $paciente->tarefas_autocuidado ? $paciente->tarefas_autocuidado : '',
+          'Teste utilizado' => $teste_utilizado,
+          'Resultado teste' => $resultado_teste ? $resultado_teste : '',
+          'Outras inf. sobre o teste' => $paciente->outras_informacao ? $paciente->outras_informacao : '',
+          'Data teste confirmatório' => $paciente->data_teste_confirmatorio ? $paciente->data_teste_confirmatorio : '',
+          'Data início sintoma' => $paciente->data_inicio_sintoma ? $paciente->data_inicio_sintoma : '',
+          'Data início monitoramento' => $paciente->data_inicio_monitoramento ? $paciente->data_inicio_monitoramento : '',
+          'Data início ac. psicológico' => $paciente->data_inicio_ac_psicologico ? $paciente->data_inicio_ac_psicologico : '',
+          'Data encerramento ac. psicológico' => $paciente->data_encerramento_ac_psicologico ? $paciente->data_encerramento_ac_psicologico : '',
+          'Data finalização caso' => $paciente->data_finalizacao_caso ? $paciente->data_finalizacao_caso : '',
+          'Auxílio emergencial' => $paciente->auxilio_emergencial ? $paciente->auxilio_emergencial : '',
+          'Descrição doenças' => $paciente->descreve_doencas ? $paciente->descreve_doencas : '',
+          'Tuberculose' => $paciente->tuberculose ? $paciente->tuberculose : '',
+          'Tabagista' => $paciente->tabagista ? $paciente->tabagista : '',
+          'Alcool crônico' => $paciente->cronico_alcool ? $paciente->cronico_alcool : '',
+          'Outras drogas' => $paciente->outras_drogas ? $paciente->outras_drogas : '',
+          'Gestante' => $paciente->gestante ? $paciente->gestante : '',
+          'Amamenta' => $paciente->amamenta ? $paciente->amamenta : '',
+          'Gestação alto risco' => $paciente->gestacao_alto_risco ? $paciente->gestacao_alto_risco : '',
+          'Pós parto' => $paciente->pos_parto ? $paciente->pos_parto : '',
+          'Data parto' => $paciente->data_parto ? $paciente->data_parto : '',
+          'Data última mestruação' => $paciente->data_ultima_mestrucao ? $paciente->data_ultima_mestrucao : '',
+          'Trimestre gestacao' => $paciente->trimestre_gestacao ? $paciente->trimestre_gestacao : '',
+          'Motivo risco gravidez' => $paciente->motivo_risco_gravidez ? $paciente->motivo_risco_gravidez : '',
+          'Data última consulta' => $paciente->data_ultima_consulta ? $paciente->data_ultima_consulta : '',
+          'Sistema saúde'  => $paciente->sistema_saude ? implode(', ', unserialize($paciente->sistema_saude)) : '',
+          'Acompanhamento UBS' => $paciente->acompanhamento_ubs ? $paciente->acompanhamento_ubs : '',
         ]);
       }
       //dd($pacientes_array);

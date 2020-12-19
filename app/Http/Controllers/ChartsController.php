d5b1f97d52eb0c154387a08ea8bd92df0c39ee8a
Alexandre Tucunduva <motive.mobi@gmail.com>
2020-12-19 08:55:25 -0300

    correção gráfico #47

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -2811,6 +2811,28 @@ class ChartsController extends Controller
 
   public function desfecho()
   {
+    /*$desfecho = DB::select("
+    SELECT
+        COALESCE(desfecho, 'Não info.') AS desfecho
+        , COALESCE(SUM(branca_sim),0) AS branca
+        , COALESCE(SUM(indigena_sim),0) AS indigena
+        , COALESCE(SUM(amarela_sim),0) AS amarela
+        , COALESCE(SUM(preta_sim)+SUM(parda_sim),0) AS negro
+        , COALESCE(SUM(nao_info_sim),0) AS nao_info
+      FROM
+        (SELECT
+          desfecho
+          , CASE WHEN cor_raca = 'Preta' THEN COUNT(pac.id) END AS preta_sim
+          , CASE WHEN cor_raca = 'Parda' THEN COUNT(pac.id) END AS parda_sim
+          , CASE WHEN cor_raca = 'Indígena' THEN COUNT(pac.id) END AS indigena_sim
+          , CASE WHEN cor_raca = 'Branca' THEN COUNT(pac.id) END AS branca_sim
+          , CASE WHEN cor_raca = 'Amarela' THEN COUNT(pac.id) END AS amarela_sim
+          , CASE WHEN cor_raca IS NULL THEN COUNT(pac.id) END AS nao_info_sim
+        FROM pacientes pac
+        LEFT JOIN quadro_atual qa ON pac.id = qa.paciente_id
+        GROUP BY cor_raca, desfecho)TB
+      GROUP BY desfecho
+    ");*/
     $desfecho = DB::select("
     SELECT
         COALESCE(desfecho, 'Não info.') AS desfecho
@@ -2830,6 +2852,7 @@ class ChartsController extends Controller
           , CASE WHEN cor_raca IS NULL THEN COUNT(pac.id) END AS nao_info_sim
         FROM pacientes pac
         LEFT JOIN quadro_atual qa ON pac.id = qa.paciente_id
+        WHERE situacao NOT IN (5, 14)
         GROUP BY cor_raca, desfecho)TB
       GROUP BY desfecho
     ");

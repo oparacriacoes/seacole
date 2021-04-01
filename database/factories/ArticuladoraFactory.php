4557c480a1df7d323187c809d942a188dcb8ced0
Douglas Silva <dogusky.silva@outlook.com>
2021-04-01 12:03:49 -0300

    altera o formato de factory para o laravel 8

---
+++ database/factories/ArticuladoraFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Articuladora;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticuladoraFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Articuladora::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name
+        ];
+    }
+}

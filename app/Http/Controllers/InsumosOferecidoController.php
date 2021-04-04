9ccfa0416b84b0ca236a61fd3d4b4757bb8b210c
Douglas Silva <dogusky.silva@outlook.com>
2021-04-04 13:18:47 -0300

    simplifica e resume processo de alterar ou criar um novo em InsumosOferecidosController

--- app/Http/Controllers/InsumosOferecidoController.php
+++ app/Http/Controllers/InsumosOferecidoController.php
@@ -2,51 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\InsumosOferecido;
-use DB;
+use App\Helpers\SerializerFields;
+use App\Http\Requests\InsumosOferecidoUpdateRequest;
+use App\Paciente;
+use Illuminate\Support\Facades\Log;
 
 class InsumosOferecidoController extends Controller
 {
-    public function store(Request $request, $id)
+    use SerializerFields;
+
+    public function store(InsumosOferecidoUpdateRequest $request, $id)
     {
-        $insumos = InsumosOferecido::where('paciente_id', $id)->first();
+        $pacient = Paciente::find($id);
+        $request_data = $this->serializerFields(['precisa_tipo_ajuda', 'tratamento_financiado', 'material_entregue'], $request->validated());
 
-        $dados = [
-      'paciente_id' => $id,
-      'condicao_ficar_isolada' => $request->condicao_ficar_isolada,
-      'tem_comida' => $request->tem_comida,
-      'tem_alguem' => $request->tem_alguem,
-      'tarefas_autocuidado' => $request->tarefas_autocuidado,
-      'precisa_tipo_ajuda' => $request->precisa_tipo_ajuda ? serialize($request->precisa_tipo_ajuda) : null,
-      'tratamento_prescrito' => $request->tratamento_prescrito,
-      'tratamento_financiado' => $request->tratamento_financiado ? serialize($request->tratamento_financiado) : null,
-      'material_entregue' => $request->material_entregue ? serialize($request->material_entregue) : null,
-      'oximetro_devolvido' => $request->oximetro_devolvido,
-    ];
-
-        if (!$insumos) {
-            DB::beginTransaction();
-            try {
-                $insumos = InsumosOferecido::create($dados);
-                DB::commit();
-                return redirect()->back()->with('success', 'Dados atualizados com sucesso.');
-            } catch (\Exception $e) {
-                DB::rollback();
-                \Log::info($e);
-                return redirect()->back()->with('error', 'Não foi  possível realizar a operação.');
-            }
-        } else {
-            DB::beginTransaction();
-            try {
-                $insumos->update($dados);
-                DB::commit();
-                return redirect()->back()->with('success', 'Dados atualizados com sucesso.');
-            } catch (\Exception $e) {
-                DB::rollback();
-                \Log::info($e);
-                return redirect()->back()->with('error', 'Não foi  possível realizar a operação.');
-            }
+        try {
+            $pacient->insumos_oferecidos()->updateOrCreate(['paciente_id' => $pacient->id], $request_data);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return back()->with('error', 'Não foi  possível realizar a operação.');
         }
+
+        return back()->with('success', 'Dados atualizados com sucesso.');
     }
 }

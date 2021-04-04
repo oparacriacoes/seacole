87f3b2aafbbd147ab48346cb5031d6540961115d
Douglas Silva <dogusky.silva@outlook.com>
2021-04-04 14:55:58 -0300

    habilita feature normalize para ser responsavel pela mormalizacao e verificacao dos dados

---
+++ resources/views/pages/normalize/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app_new')
+@section('content')
+
+<div class="app-main__inner">
+  <div class="main-card mb-3 card">
+    <div class="card-body">
+
+    </div>
+  </div>
+
+</div>
+@endsection

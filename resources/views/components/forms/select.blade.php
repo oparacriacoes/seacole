98b57b58a53b21f23485c384af5ba0594abc08a7
Douglas Silva <dogusky.silva@outlook.com>
2021-04-12 03:16:16 -0300

    cria component para select

---
+++ resources/views/components/forms/select.blade.php
@@ -0,0 +1,13 @@
+@props([
+    'items' => [],
+    'property' => [],
+    'value' => null,
+])
+
+{{$value}}
+<select type="select" id="{{$property}}" name="{{$property}}" class="custom-select">
+    <option value="">Selecione</option>
+    @foreach($items as $key => $content)
+        <option value="{{$key}}" @if($key == old($property, $value)) selected @endif>{{$content}}</option>
+    @endforeach
+</select>

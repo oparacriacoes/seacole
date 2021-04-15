8b728d7c70f1616dbd488f779c219f4e30a299c9
Douglas Silva <dogusky.silva@outlook.com>
2021-04-15 00:55:31 -0300

    renomeio PacienteStoreRequest para PacienteRequest
    uma vez que ambos formulários terao todos os campos editaveis logo um Request Validation serve para criar ou alterar

--- app/Http/Requests/PacienteRequest.php
+++ app/Http/Requests/PacienteRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Support\Str;
 use Illuminate\Foundation\Http\FormRequest;
 
-class PacienteStoreRequest extends FormRequest
+class PacienteRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.

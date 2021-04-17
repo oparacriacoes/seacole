8e43c84a2135d08feec241299eed4742dd72cafc
Douglas Silva <dogusky.silva@outlook.com>
2021-04-17 16:46:24 -0300

    finaliza os components da tela de insumos

--- resources/views/pages/paciente/_forms_edit/insumos.blade.php
+++ resources/views/pages/paciente/_forms_edit/insumos.blade.php
@@ -23,6 +23,37 @@
         </div>
     </div>
 
+    <div class="divider"></div>
+
+    <div class="form-row">
+        <div class="col-md-4">
+            <x-forms.choices.tipo-ajuda :value="$insumos->precisa_tipo_ajuda ?? []" />
+        </div>
+
+        <div class="col-md-4">
+            <x-forms.choices.yes-or-not property="tratamento_prescrito" :value="$insumos->tratamento_prescrito">
+                Tratamento foi prescrito por algum médico do projeto?
+            </x-forms.choices.yes-or-not>
+        </div>
+
+        <div class="col-md-4">
+            <x-forms.choices.tratamento-financiado :value="$insumos->tratamento_financiado ?? []" />
+        </div>
+    </div>
+
+    <div class="divider"></div>
+
+    <div class="form-row">
+        <div class="col-md-12 mb-1">
+            <x-forms.choices.material-entregue :value="$insumos->material_entregue ?? []" />
+        </div>
+        <div class="col-md-12">
+            <x-forms.choices.yes-or-not property="oximetro_devolvido" :value="$insumos->oximetro_devolvido">
+                Se o caso já tiver sido encerrado: oxímetro foi devolvido?
+            </x-forms.choices.yes-or-not>
+        </div>
+    </div>
+
     <div class="position-relatives row fdorm-check">
         <div class="col-sm-12 offset-sm-2s pt-3">
             <button class="btn btn-success">Salvar</button>

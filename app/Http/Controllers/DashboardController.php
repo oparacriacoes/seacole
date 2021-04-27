417b98d6dc2e3b8ed8170c0306a2aef9c569c4a6
Douglas Silva <dogusky.silva@outlook.com>
2021-04-26 21:59:16 -0300

    adiciona as primeiras informacoes para o dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Enums\DesfechosEnum;
+use App\Enums\SintomasIniciais;
+use App\Paciente;
+use App\QuadroAtual;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.dashboard');
+        $pacientes_total = Paciente::count();
+        $casos_confirmado = Paciente::where('sintomas_iniciais', SintomasIniciais::CONFIRMADO)->count();
+        $total_obitos = QuadroAtual::where('desfecho', DesfechosEnum::OBITO_COVID)->count();
+
+        return view('pages.dashboard', [
+            'pacientes_total' => $pacientes_total,
+            'casos_confirmado' => $casos_confirmado,
+            'total_obitos' => $total_obitos
+        ]);
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,19 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="app-main__inner">
-        <div class="app-page-title">
-            <div class="page-title-wrapper">
-                <div class="page-title-heading">
-                    <div class="page-title-icon">
-                        <i class="pe-7s-car icon-gradient bg-mean-fruit">
-                        </i>
+<div class="app-main__inner">
+    <div class="app-page-title">
+        <div class="page-title-wrapper">
+            <div class="page-title-heading">
+                <div class="page-title-icon">
+                    <i class="pe-7s-graph text-primary">
+                    </i>
+                </div>
+                <div>Dashboard Analítico
+                    <div class="page-title-subheading">This is an example dashboard created using build-in elements and components.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 col-xl-4">
+            <div class="card mb-3 widget-content bg-primary">
+                <div class="widget-content-wrapper text-white">
+                    <div class="widget-content-left">
+                        <div class="widget-heading">Pacientes</div>
+                        <div class="widget-subheading">Total cadastrado</div>
                     </div>
-                    <div>Dashboard
-                      <div class="page-title-subheading">This is an example dashboard created using build-in elements and components.</div>
+                    <div class="widget-content-right">
+                        <div class="widget-numbers text-white"><span>{{$pacientes_total}}</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-xl-4">
+            <div class="card mb-3 widget-content bg-info">
+                <div class="widget-content-wrapper text-white">
+                    <div class="widget-content-left">
+                        <div class="widget-heading">Diagnósticos Covid-19</div>
+                        <div class="widget-subheading">Casos Confirmado</div>
+                    </div>
+                    <div class="widget-content-right">
+                        <div class="widget-numbers text-white"><span>{{$casos_confirmado}}</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-xl-4">
+            <div class="card mb-3 widget-content bg-danger">
+                <div class="widget-content-wrapper text-white">
+                    <div class="widget-content-left">
+                        <div class="widget-heading">Óbitos</div>
+                        <div class="widget-subheading">Decorrência do Covid-19</div>
+                    </div>
+                    <div class="widget-content-right">
+                        <div class="widget-numbers text-white"><span>{{$total_obitos}}</span></div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection

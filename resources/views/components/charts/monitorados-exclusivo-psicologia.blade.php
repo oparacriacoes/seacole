50dd6e7f6816c6dd0db067acc316a43734ed30bf
Douglas Silva <dogusky.silva@outlook.com>
2021-05-05 21:41:54 -0300

    adiciona dados para gráfico MonitoradosExclusivoPsicologia

--- resources/views/components/charts/monitorados-exclusivo-psicologia.blade.php
+++ resources/views/components/charts/monitorados-exclusivo-psicologia.blade.php
@@ -1,3 +1,39 @@
-<div>
-    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
-</div>
+<script type="text/javascript">
+    const chart_data = @json($chart_data);
+
+    console.log(chart_data)
+
+    const ctx = document.getElementById('chartjs').getContext('2d');
+
+    let chartjs = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: chart_data.labels,
+            datasets: [{
+                label: 'Pacientes Monitorados x Paciente Exclusivo Psicologia',
+                data: chart_data.data,
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }],
+        },
+        options: {
+            plugins: {
+                datalabels: {
+                    backgroundColor: 'rgb(75, 192, 192)',
+                    borderRadius: 4,
+                    color: 'white',
+                    font: {
+                        weight: 'bold'
+                    },
+                    formatter: (value, ctx) => {
+                        let sum = ctx.dataset._meta[0].total;
+                        let percentage = (value * 100 / sum).toFixed(2) + "%";
+                        return percentage;
+                    },
+                    padding: 6
+                }
+            }
+        }
+    });
+</script>

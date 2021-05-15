26b25ba9093246ed9377c7a6b181c5dc38a04ec4
Douglas Silva <dogusky.silva@outlook.com>
2021-05-15 16:03:22 -0300

    feat: chart em barras para Faixa Etária por Gênero

---
+++ resources/views/components/charts/faixa-etaria-por-genero-b.blade.php
@@ -0,0 +1,42 @@
+<script type="text/javascript">
+    const chart_data = @json($chart_data);
+
+    const ctx = document.getElementById('chartjs').getContext('2d');
+
+    let chartjs = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: chart_data.labels,
+            datasets: chart_data.datasets,
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'FAIXA ETÁRIA POR GÊNERO',
+                position: 'top'
+            },
+            legend: {
+                position: 'bottom',
+            },
+            scales: {
+                xAxes: [{
+                    stacked: true
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            },
+            plugins: {
+                datalabels: {
+                    borderRadius: 4,
+                    color: 'black',
+                    font: {
+                        weight: 'normal'
+                    },
+                    padding: 2,
+                    formatter: (value, ctx) => value != '0' ? Math.abs(value) : '',
+                }
+            }
+        }
+    });
+</script>

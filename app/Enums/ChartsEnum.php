8971923b132ce09030d134955f9058e86b713e68
Douglas Silva <dogusky.silva@outlook.com>
2021-05-15 21:09:03 -0300

    feat: gráfico para renda per capta para raça cor

--- app/Enums/ChartsEnum.php
+++ app/Enums/ChartsEnum.php
@@ -19,8 +19,8 @@ final class ChartsEnum extends ReadableEnum
     public const GRAFICO_10 = 'faixa-etaria-por-raca-cor';
     public const GRAFICO_11 = 'pessoas-residencia-raca-cor';
     public const GRAFICO_12_A = 'classe-social-por-renda-familiar';
+    public const GRAFICO_12_B = 'classe-social-renda-per-capta-raca-cor';
 
-    public const GRAFICO_13 = '13';
     public const GRAFICO_14 = '14';
     public const GRAFICO_15 = '15';
     public const GRAFICO_16 = '16';
@@ -84,7 +84,7 @@ final class ChartsEnum extends ReadableEnum
             self::GRAFICO_10,
             self::GRAFICO_11,
             self::GRAFICO_12_A,
-            // self::GRAFICO_13,
+            self::GRAFICO_12_B,
             // self::GRAFICO_14,
             // self::GRAFICO_15,
             // self::GRAFICO_16,
@@ -150,8 +150,8 @@ final class ChartsEnum extends ReadableEnum
             self::GRAFICO_10 => Str::title('10 FAIXA ETÁRIA POR RAÇA COR'),
             self::GRAFICO_11 => Str::title('11 NÚMERO DE PESSOAS/RESIDÊNCIA POR RAÇA/COR'),
             self::GRAFICO_12_A => Str::title('12a CLASSE SOCIAL POR RENDA BRUTA FAMILIAR'),
+            self::GRAFICO_12_B => Str::title('12b CLASSE SOCIAL RENDA PER-CAPTA POR RAÇA/COR'),
 
-            // self::GRAFICO_13 => Str::title('13'),
             // self::GRAFICO_14 => Str::title('14'),
             // self::GRAFICO_15 => Str::title('15'),
             // self::GRAFICO_16 => Str::title('16'),
---
+++ app/View/Components/Charts/ClasseSocialRendaPerCaptaRacaCor.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\View\Components\Charts;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class ClasseSocialRendaPerCaptaRacaCor extends ChartComponent
+{
+    protected string $componentView = 'components.charts.classe-social-renda-per-capta-raca-cor';
+
+    public function chartData(): array
+    {
+        $collection = collect(DB::select($this->query));
+
+        $datasets = [];
+        $labels = array_filter(array_keys((array)$collection->first()), function ($label) {
+            return $label != 'label';
+        });
+
+        foreach ($labels as $label) {
+            array_push($datasets, [
+                'label' => (string)Str::of($label)->replace('_', ' ')->title(),
+                'data' => $collection->pluck($label)
+            ]);
+        }
+
+        return [
+            'labels' => $collection->pluck('label'),
+            'datasets' => $datasets,
+        ];
+    }
+
+    private $query = "
+        SELECT
+            renda_residencia as label,
+            COALESCE(SUM(sem_informacao), 0) AS sem_informacao,
+            COALESCE(SUM(preta), 0) AS preta,
+            COALESCE(SUM(parda), 0) AS parda,
+            COALESCE(SUM(branca), 0) AS branca,
+            COALESCE(SUM(amarela), 0) AS amarela,
+            COALESCE(SUM(indigena), 0) AS indigena
+        FROM
+            (
+            SELECT
+                CASE
+                    WHEN renda_residencia / numero_pessoas_residencia > 0
+                    AND renda_residencia / numero_pessoas_residencia <= 1254 THEN 'Classe E'
+                    WHEN renda_residencia / numero_pessoas_residencia >= 1255
+                    AND renda_residencia / numero_pessoas_residencia <= 2004 THEN 'Classe D'
+                    WHEN renda_residencia / numero_pessoas_residencia >= 2005
+                    AND renda_residencia / numero_pessoas_residencia <= 8640 THEN 'Classe C'
+                    WHEN renda_residencia / numero_pessoas_residencia >= 8641
+                    AND renda_residencia / numero_pessoas_residencia <= 11261 THEN 'Classe B'
+                    WHEN renda_residencia / numero_pessoas_residencia >= 11262 THEN 'Classe A'
+                    ELSE 'Sim inf.'
+                END AS renda_residencia,
+                CASE
+                    WHEN cor_raca IS NULL THEN COUNT(id)
+                END AS sem_informacao,
+                CASE
+                    WHEN cor_raca = 'Preta' THEN COUNT(id)
+                END AS preta,
+                CASE
+                    WHEN cor_raca = 'Parda' THEN COUNT(id)
+                END AS parda,
+                CASE
+                    WHEN cor_raca = 'Branca' THEN COUNT(id)
+                END AS branca,
+                CASE
+                    WHEN cor_raca = 'Amarela' THEN COUNT(id)
+                END AS amarela,
+                CASE
+                    WHEN cor_raca = 'Indígena' THEN COUNT(id)
+                END AS indigena
+            FROM
+                pacientes
+            GROUP BY renda_residencia, numero_pessoas_residencia, cor_raca
+            ) TBB
+        GROUP BY renda_residencia
+        ORDER BY renda_residencia;
+    ";
+}

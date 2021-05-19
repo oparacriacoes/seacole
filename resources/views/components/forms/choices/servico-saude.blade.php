5bca875447d17185e3c93a407745c941f532e288
Douglas Silva <dogusky.silva@outlook.com>
2021-05-19 18:47:07 -0300

    fix: corrige labels para uso

--- resources/views/components/forms/choices/servico-saude.blade.php
+++ resources/views/components/forms/choices/servico-saude.blade.php
@@ -1,6 +1,6 @@
 <div class="position-relative form-group">
     <label for="resultado_teste" class="">
-        A pessoa precisou ir a algum serviço de saúde?
+        {{ $slot }}
     </label>
     <br>
     <x-forms.check-group :items="$servicos_saude" :property="$property" :value="$value" />

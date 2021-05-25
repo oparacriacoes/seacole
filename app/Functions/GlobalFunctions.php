8ea5df2e820b027aa058776b3508c502db3ef359
Douglas Silva <dogusky.silva@outlook.com>
2021-05-25 14:04:51 -0300

    feat: funcoes auxiliares para montar os dados dos gráficos

--- app/Functions/GlobalFunctions.php
+++ app/Functions/GlobalFunctions.php
@@ -12,3 +12,28 @@ function human_boolean($value): ?string
 
     throw new Exception("Por favor insira apenas valores booleanos ou null", 1);
 }
+
+function join_colors_to_black($collection)
+{
+    return $collection->pluck('cor_raca')
+        ->filter(function ($cor_raca) {
+            return $cor_raca != 'Parda';
+        })
+        ->map(function ($cor_raca) {
+            return $cor_raca == 'Preta' ? 'Negra' : $cor_raca;
+        })->values();
+}
+
+function stack_black_colors(array $datasets)
+{
+    return array_map(function ($dataset) {
+        $dataset['xAxisID'] = 'xAxis1';
+        if ($dataset['label'] == 'Preta' || $dataset['label'] == 'Parda') {
+            $dataset['stack'] = 'Stack Negra';
+        } else {
+            $dataset['stack'] = 'Stack ' . $dataset['label'];
+        }
+
+        return $dataset;
+    }, $datasets);
+}
